7510a368efff7c3822a56a783bc25a0b0a1528a2
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-01-22 17:44:11 -0500

    completed closure problem set

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,26 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+//function printMyName(first, last) {
+//let sentence = `Hello, my name is`;
 
+//function wholeName() {
+//return `${sentence} ${first} ${last}`;
+//}
 
+//return wholeName();
+//}
+
+//console.log(printMyName("Jon", "Palacio"));
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+//let count = 0;
+//const counter = inc => {
+//count++;
+//return count;
+//// Return a function that when invoked increments and returns a counter variable.
+//};
+//console.log(counter());
+//console.log(counter());
+//console.log(counter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
